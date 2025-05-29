58dc4dfe8201a370a4011952ecfdce59fcd3fcf1
G384512023 <238451@st.takushoku-u.ac.jp>
2025-05-29 13:44:35 +0900

    あ

--- practice-eventdom/input-demo.js
+++ practice-eventdom/input-demo.js
@@ -1,7 +1,8 @@
 function greeting(){
    let i = document.querySelector('input[name="shimei"]');
    shimei = i.value;
-   let p = document.querySelector('');
+   
+   console.log = ('shimei');
   }
   let b = document.querySelector('button#print');
   b.addEventListener('click', greeting);
